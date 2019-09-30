86c0584dfed5ac4c06fcde513abe87b61cbb3834
MadJacksson <55533084+MadJacksson@users.noreply.github.com>
2019-09-30 10:35:07 +0200

    test
    test för github

--- Rebec/Rebec.c
+++ Rebec/Rebec.c
@@ -15,16 +15,28 @@ int main(void)
 	//TODO Place here your code
 	setAllSensorMode(TOUCH_PRESS, NO_SEN, TOUCH_PRESS, US_DIST_MM);
 	readSensor(IN_1);
+	readSensor(IN_3);
 		if(readSensor(IN_1) == 1)
 		{
-			LcdPrintf(1, "Hello World!\n");
+			LcdPrintf(1, "FIRST IS PRESSED!\n");
 			Wait(SEC_2);
 		}
 		else {
-				LcdPrintf(1, "NOPE!\n");
+				LcdPrintf(1, "NOPE!\n"); //det här är ett test
 				Wait(SEC_2);
 		}
 
+		if(readSensor(IN_3) == 1)
+		{
+			LcdPrintf(1, "SECOND IS PRESSED!\n");
+			Wait(SEC_2);
+		}
+
+		if(readSensor(IN_3)+readSensor(IN_1)==1)
+		{
+			LcdPrintf(1, "BOTH ARE PRESSED!\n");
+			Wait(SEC_2);
+		}
 
 	FreeEV3();
 	return 0;
