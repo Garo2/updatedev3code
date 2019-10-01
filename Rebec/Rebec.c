d236a34f623b11225e2a002ae5a8028f02363c2e
MadJacksson <55533084+MadJacksson@users.noreply.github.com>
2019-10-01 21:48:28 +0200

    touch sesnor fwd
    nu åker den frammåt till den nuddar väggen, justerar sig rakt och följer intruktioner om att backa, vända och köra

--- Rebec/Rebec.c
+++ Rebec/Rebec.c
@@ -17,21 +17,41 @@ int main(void)
 	readSensor(IN_1);
 	readSensor(IN_3);
 
+	while(readSensor(IN_1) == 0, readSensor(IN_3) == 0)
+	{
+		LcdPrintf(1,"Going Forward! \n");
+				OnRevSync(OUT_AB,75);
+				Wait(SEC_1);
+	}
+
 	while(readSensor(IN_1) == 1, readSensor(IN_3) == 0)
 	{
 		LcdPrintf(1, "FIRST IS PRESSED!\n");
+		LcdPrintf(1,"Turn Right! \n");
+		OnRevSync(OUT_B,23);
 		Wait(SEC_1);
 	}
 
 	while(readSensor(IN_3) == 1, readSensor(IN_1) == 0)
 	{
 		LcdPrintf(1, "SECOND IS PRESSED!\n");
+		LcdPrintf(1,"Turn Right!\n");
+		OnRevSync(OUT_A,23);
 		Wait(SEC_1);
 	}
 
 	if(readSensor(IN_3) && readSensor(IN_1)==1)
 	{
-		LcdPrintf(1, "BOTH ARE PRESSED!\n");
+		LcdPrintf(1,"Going Backward!\n");
+		OnFwdReg(OUT_AB,75);
+		Wait(SEC_2);
+
+		LcdPrintf(1,"Turn Left! 1st \n");
+		OnFwdReg(OUT_B,23);
+		Wait(SEC_4);
+
+		LcdPrintf(1,"Going Forward! \n");
+		OnRevSync(OUT_AB,75);
 		Wait(SEC_3);
 			}
 
