1606a495da30c37b4a5801570d29c9bfe492eb31
MadJacksson <55533084+MadJacksson@users.noreply.github.com>
2019-10-01 21:05:03 +0200

    touch sensor
    nu reagerar inte in_1 och in_2 separat, utan de kommer stanna och sätta igång in_1 && in_2

--- Rebec/Rebec.c
+++ Rebec/Rebec.c
@@ -16,27 +16,24 @@ int main(void)
 	setAllSensorMode(TOUCH_PRESS, NO_SEN, TOUCH_PRESS, US_DIST_MM);
 	readSensor(IN_1);
 	readSensor(IN_3);
-		if(readSensor(IN_1) == 1)
-		{
-			LcdPrintf(1, "FIRST IS PRESSED!\n");
-			Wait(SEC_2);
-		}
-		else {
-				LcdPrintf(1, "NOPE!\n"); //det här är ett test
-				Wait(SEC_2);
-		}
 
-		if(readSensor(IN_3) == 1)
-		{
-			LcdPrintf(1, "SECOND IS PRESSED!\n");
-			Wait(SEC_2);
-		}
+	while(readSensor(IN_1) == 1, readSensor(IN_3) == 0)
+	{
+		LcdPrintf(1, "FIRST IS PRESSED!\n");
+		Wait(SEC_1);
+	}
 
-		if(readSensor(IN_3)+readSensor(IN_1)==1)
-		{
-			LcdPrintf(1, "BOTH ARE PRESSED!\n");
-			Wait(SEC_2);
-		}
+	while(readSensor(IN_3) == 1, readSensor(IN_1) == 0)
+	{
+		LcdPrintf(1, "SECOND IS PRESSED!\n");
+		Wait(SEC_1);
+	}
+
+	if(readSensor(IN_3) && readSensor(IN_1)==1)
+	{
+		LcdPrintf(1, "BOTH ARE PRESSED!\n");
+		Wait(SEC_3);
+			}
 
 	FreeEV3();
 	return 0;
