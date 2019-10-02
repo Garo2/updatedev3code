88f10ed79943ec9b0ece00f0da9ad15a9ffbb145
veralindstrom <32744192+veralindstrom@users.noreply.github.com>
2019-10-02 17:16:45 +0200

    16 loop

--- Ev3for-backward/connectToSonicSenor.c
+++ Ev3for-backward/connectToSonicSenor.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ev3.h>
 
-void rotateMotor(int l);
+void rotateMotor();
 int findMinDistance();
 void forward();
 void touchSensor();
@@ -9,41 +9,53 @@ void touchSensor();
 int main() {
 
 	InitEV3();
-	// setAllSensorMode(TOUCH_PRESS, NO_SEN, TOUCH_PRESS, US_DIST_MM);
+    setAllSensorMode(TOUCH_PRESS, NO_SEN, TOUCH_PRESS, US_DIST_MM);
+
+
     int indexTid = findMinDistance();
     int i;
 
-    for(i = 0; i < indexTid; i++) {
-        rotateMotor(i);
+    for(i = 0; i <= indexTid; i++) {
+        LcdPrintf(1, "%d, ", i);
+        rotateMotor();
     }
-    Off(OUT_A);
 
-   // forward();
+    forward();
 
-   // touchSensor();
 
+   // Off(OUT_A);
+
+	FreeEV3();
     return 0;
 }
 
+
+
+void rotateMotor () {
+	  OnFwdReg(OUT_B, 30);
+	    OnRevReg(OUT_A, 30);
+	    Wait(MS_600 + MS_50);
+}
+
 int findMinDistance () {
-    int distance;
     int minDis;
-    int arrayDis[8];
+    int arrayDis[15];
     int indexTidForMinDis;
     int j;
-   
+
 
     setAllSensorMode(TOUCH_PRESS, NO_SEN, TOUCH_PRESS, US_DIST_MM);
+    readSensor(IN_4);
 
-    for(j = 0; j < 9; j++) {
-        rotateMotor(j);
-        distance = readSensor(IN_4);
-        arrayDis[j] = distance;
-        LcdPrintf(1, "Avståndet Är: %d mm", arrayDis[j]);
+    for(j = 0; j <= 15; j++) {
+        rotateMotor();
+        arrayDis[j] = readSensor(IN_4);
+        LcdPrintf(1, "%d: %d ", j, arrayDis[j]);
     }
 
     minDis = arrayDis[0];
-    for(j = 1; j<8; j++)
+
+    for(j = 1; j <= 15; j++)
     {
         if(minDis > arrayDis[j])
         {
@@ -51,71 +63,13 @@ int findMinDistance () {
             indexTidForMinDis = j;
         }
     }
-
     return indexTidForMinDis;
-}
-
-void rotateMotor (int l) {
-    LcdPrintf(1, "Turn Right! %d \n", l);
-    OnFwdReg(OUT_A, 15);
-    OnRevReg(OUT_B, 15);
-    Wait(SEC_2 + MS_100);
-
 }
 
 void forward() {
 	//framåt X m
     LcdPrintf(1, "Going Forward!\n");
     OnRevSync(OUT_AB,75);
-    Wait(SEC_4);
+    Wait(SEC_20);
 }
 
-void touchSensor() {
-	setAllSensorMode(TOUCH_PRESS, NO_SEN, TOUCH_PRESS, US_DIST_MM);
-		readSensor(IN_1);
-		readSensor(IN_3);
-			if(readSensor(IN_1) == 1){
-				LcdPrintf(1,"Turn Right!n");
-				OnRevSync(OUT_B,23);
-				Wait(SEC_7 + MS_500);
-
-				if(readSensor(IN_1) && readSensor(IN_3) == 1){
-					Off(OUT_B);
-					 LcdPrintf(1, "Going Backward!\n");
-					    OnFwdSync(OUT_AB,75);
-					    Wait(SEC_4);
-
-					 LcdPrintf(1, "Turn 90 degrees!\n");
-					    OnFwdReg(OUT_A, 44);
-					    Wait(SEC_3 + MS_700 + MS_50);
-				}
-			}
-			else if(readSensor(IN_3) == 1){
-					LcdPrintf(1,"Turn Left!\n");
-					OnRevSync(OUT_A,23);
-					Wait(SEC_7 + MS_500);
-
-					if(readSensor(IN_1) && readSensor(IN_3) == 1){
-						Off(OUT_B);
-						 LcdPrintf(1, "Going Backward!\n");
-						   OnFwdSync(OUT_AB,75);
-						   Wait(SEC_4);
-
-						 LcdPrintf(1, "Turn 90 degrees!\n");
-							OnFwdReg(OUT_A, 44);
-							Wait(SEC_3 + MS_700 + MS_50);
-					}
-			}
-
-			if(readSensor(IN_1) && readSensor(IN_3) == 1)
-				{
-					Off(OUT_AB);
-					 LcdPrintf(1, "Going Backward!\n");
-					  OnFwdSync(OUT_AB,75);
-					  Wait(SEC_4);
-
-					 LcdPrintf(1, "Turn 90 degrees!");
-					   OnFwdReg(OUT_A, 44);
-					   Wait(SEC_3 + MS_700 + MS_50);
-				}
-}
